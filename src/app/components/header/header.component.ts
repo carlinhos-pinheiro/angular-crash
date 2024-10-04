29fc2a15591eee398a0d0f81ff30474aef1a6a74
F1803011 Carlos Daniel Pinheiro Dos Santos <carlos.santos.011@bb.com.br>
2024-10-04 14:07:32 -0300

    started

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Task } from './../../Task';
+import { Component } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+  toggleAddTask() : void{
+    console.log("Toggled");
+  }
+
+}
